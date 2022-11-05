eeb381764b1735662bae3e8042f7d1d1863f8abe
Josh Justice <me@codingitwrong.com>
2022-11-05 12:20:05 -0400

    Extract header

--- src/Layout.js
+++ src/Layout.js
@@ -1,24 +1,10 @@
-import {Link, Outlet} from 'react-router-dom';
-import {useToken} from './data/token';
+import {Outlet} from 'react-router-dom';
+import Header from './Header';
 
 export default function Layout() {
-  const {clearToken} = useToken();
-
   return (
     <>
-      <button type="button" onClick={clearToken}>
-        Sign Out
-      </button>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Unread</Link>
-          </li>
-          <li>
-            <Link to="/links/read">Read</Link>
-          </li>
-        </ul>
-      </nav>
+      <Header />
       <Outlet />
     </>
   );
