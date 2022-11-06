9013c31ae6165f62bae781ba800010b2f3018992
Josh Justice <me@codingitwrong.com>
2022-11-06 07:22:30 -0500

    Use custom webpack html

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const container = document.createElement('div');
-document.body.appendChild(container);
+const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
 
 root.render(
--- webpack.common.js
+++ webpack.common.js
@@ -9,6 +9,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Firehose',
+      template: './src/index.html',
     }),
   ],
   output: {
