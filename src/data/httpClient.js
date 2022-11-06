0e419f173121bcf2ada5983f9a69e2069dff1ef8
Josh Justice <me@codingitwrong.com>
2022-11-06 08:07:23 -0500

    Replace Axios with custom HTTP client

--- src/data/httpClient.js
+++ src/data/httpClient.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import baseUrl from '../baseUrl';
 
 export default function httpClient({token} = {}) {
@@ -6,8 +5,55 @@ export default function httpClient({token} = {}) {
   if (token) {
     headers.Authorization = `Bearer ${token}`;
   }
-  return axios.create({
-    baseURL: baseUrl,
-    headers,
-  });
+  return new HttpClient({baseUrl, headers});
+}
+
+class HttpClient {
+  constructor({baseUrl, headers}) {
+    this.baseUrl = baseUrl;
+    this.headers = headers;
+  }
+
+  async get(url) {
+    const response = await fetch(this.#fullUrl(url), {
+      method: 'GET',
+      headers: this.headers,
+    });
+    const data = await response.json();
+    return {data};
+  }
+
+  async post(url, body, {headers}) {
+    const response = await fetch(this.#fullUrl(url), {
+      method: 'POST',
+      headers: {...this.headers, ...headers},
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return {data};
+  }
+
+  async patch(url, body, {headers}) {
+    const response = await fetch(this.#fullUrl(url), {
+      method: 'PATCH',
+      headers: {...this.headers, ...headers},
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return {data};
+  }
+
+  async delete(url, body, {headers}) {
+    const response = await fetch(this.#fullUrl(url), {
+      method: 'DELETE',
+      headers: {...this.headers, ...headers},
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return {data};
+  }
+
+  #fullUrl(url) {
+    return `${this.baseUrl}/${url}`;
+  }
 }
