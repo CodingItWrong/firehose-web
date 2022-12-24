6e805d644f6bb8114fc2e0f9938a61351106c86c
Josh Justice <me@codingitwrong.com>
2022-12-24 06:24:25 -0500

    Fix lint

--- src/routes/links/BookmarkDetail/BookmarkDetailForm.js
+++ src/routes/links/BookmarkDetail/BookmarkDetailForm.js
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import ButtonGroup from '../../../components/ButtonGroup';
 
 export default function BookmarkDetailForm({attributes, onSave, cancelPath}) {
